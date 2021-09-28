9a718f928c5b46a707ffc7b892de2d803cebcba6
Tom Abel <tabel@stanford.edu>
2021-09-28 12:21:18 -0700

    initial commit

---
+++ kNN_CDFs.py
@@ -0,0 +1,24 @@
+import numpy as np
+import scipy.spatial
+import sys
+sys.path.append('/Users/tabel/Research/codes/SEdist/')
+from SEdist import SE_distribution
+
+def VolumekNN(xin, xout, k=1, periodic=0):
+    if isinstance(k, int): k = [k] # 
+    dim = xin.shape[1]
+    xtree = scipy.spatial.cKDTree(xin, boxsize=periodic)
+    dis, disi = xtree.query(xout, k=k, n_jobs=-1)
+    vol = np.empty_like(dis) # same shape as distance including all k values
+    Cr = [2, np.pi, 4 * np.pi / 3][dim - 1]  # Volume prefactor for 1,2, 3D
+    for c, k in enumerate(np.nditer(np.array(k))):
+        vol[:,c] = Cr * dis[:,c]**dim
+    return vol
+    
+def CDFkNN(xin, xout, kneighbors=1, periodic=0,compress="none",Ninterpolants=500):
+    vol = VolumekNN(xin, xout, k=kneighbors, periodic=periodic)
+    for c, k in enumerate(np.nditer(np.array(kneighbors))):
+        vol[:,c] = np.sort(vol[:,c])
+    N = len(vol)
+    cdfs = [SE_distribution(vol[:,k],compress=compress,Ninterpolants=Ninterpolants) for k in kneighbors]
+    return cdfs
